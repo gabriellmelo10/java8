ce5a2282de80a027d97853c9daa209d510bba661
= <=>
2022-08-25 11:10:17 -0300

    3ª forma lamda

--- br/com/Main.java
+++ br/com/Main.java
@@ -1,24 +1,15 @@
 package br.com.codandosimples;
 
+@FunctionalInterface
 interface Taxi {
     void reserva();
 }
 
-/**
- * Implementando classe anonima
- */
 public class Main {
-
     public static void main(String[] args) {
-        Taxi taxi = new Taxi() {
-            @Override
-            public void reserva() {
-                System.out.println("Taxi reservado");
-            }
+        Taxi taxi = () -> {
+            System.out.println("Taxi reservado");
         };
         taxi.reserva();
     }
 }
-
-
-
